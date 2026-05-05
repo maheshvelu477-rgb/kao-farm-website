51c2244a32976d92d79fcd57bb9fe4f3b937b49d
maheshvelu477-rgb <maheshvelu477@gmail.com>
2026-05-05 15:21:09 +0530

    Updated

--- server.js
+++ server.js
@@ -47,7 +47,15 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://kao-farm-website.netlify.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json({ limit: "10mb" })); // for image base64
 
  

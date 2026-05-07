dddad65aa097f58f9fbc1c03e46d83eeefaa81ce
maheshvelu477-rgb <maheshvelu477@gmail.com>
2026-05-07 18:39:06 +0530

    Server Updated

--- config/db.js
+++ config/db.js
@@ -1,17 +1,86 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+
+//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`❌ MongoDB Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+
+    console.log("MONGO_URI =", process.env.MONGO_URI);
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+
   } catch (error) {
-    console.error(`❌ MongoDB Error: ${error.message}`);
+
+    console.log("FULL ERROR =>", error);
+
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // // const mongoose = require("mongoose");

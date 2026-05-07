a568052859d8459f022c5c3139e5bb72c171550e
maheshvelu477-rgb <maheshvelu477@gmail.com>
2026-05-07 19:29:43 +0530

    Server Updated

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Start server FIRST, then connect to DB
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
   console.log("EMAIL_USER:", process.env.EMAIL_USER);
